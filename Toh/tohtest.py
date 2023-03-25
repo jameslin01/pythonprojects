29dd2c1340a89af253fc51d4e1d1ed2a5141a333
James Lin <arigatojames@gmail.com>
2023-03-25 02:25:10 +0000

    1

--- Toh/tohtest.py
+++ Toh/tohtest.py
@@ -15,8 +15,25 @@ moves = 0
 bg = pygame.image.load('tohgraphics/bg.jpeg').convert()
 bg = pygame.transform.scale(bg, (800,400))
 
-board = pygame.Surface((150,50))
-board.fill('Gold')
+board_surf = pygame.Surface((720,25))
+board_surf.fill('#c0e8ec')
+board_rect = board_surf.get_rect(center = (400,345))
+
+
+tower1_surf = pygame.Surface((10,200))
+tower1_surf.fill('Silver')
+tower1_rect = tower1_surf.get_rect(midbottom = (160, 335))
+
+tower2_surf = pygame.Surface((10,200))
+tower2_surf.fill('Silver')
+tower2_rect = tower1_surf.get_rect(midbottom = (400, 335))
+
+tower3_surf = pygame.Surface((10,200))
+tower3_surf.fill('Silver')
+tower3_rect = tower1_surf.get_rect(midbottom = (640, 335))
+
+
+
 
 
 def display_intro():
@@ -36,13 +53,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        
-        mouse_pos = pygame.mouse.get_pos()
 
-        if event.type == pygame.MOUSEMOTION:
-            pos_surf = test_font.render('Pos: ' + f'{mouse_pos}', False, (64,64,64))
-            pos_rect = pos_surf.get_rect(center = (400,350))
-            screen.blit(pos_surf, pos_rect)
 
 
     # Events for keys
@@ -51,7 +62,20 @@ while True:
         
         screen.blit(bg, (0,0))
 
-        pygame.draw.rect(screen, '#c0e8ec', pygame.Rect(80,100,100,80))
+        # pygame.draw.rect(screen, '')
+        screen.blit(board_surf, board_rect)
+        screen.blit(tower1_surf, tower1_rect)
+        screen.blit(tower2_surf, tower2_rect)
+        screen.blit(tower3_surf, tower3_rect)
+
+        # Get coordinates based on where your mouse is
+
+        mouse_pos = pygame.mouse.get_pos()
+
+        if event.type == pygame.MOUSEMOTION:
+            pos_surf = test_font.render('Pos: ' + f'{mouse_pos}', False, (64,64,64))
+            pos_rect = pos_surf.get_rect(center = (400,350))
+            screen.blit(pos_surf, pos_rect)
     
     else:
         
