8cdd9f94b02842125d4ea7923dc7ef0503511d07
James Lin <arigatojames@gmail.com>
2023-04-28 10:59:29 +0100

    1

--- Budgeting app/app.py
+++ Budgeting app/app.py
@@ -1,5 +1,11 @@
+# Importing tkinter
+
 from tkinter import *
 from tkinter import Tk, ttk
+
+# Importing pillow
+
+from PIL import Image, ImageTk
 
 # Colours
 
@@ -16,16 +22,47 @@ c9 = "#e9edf5"
 c10 = "#6e8faf"
 c11 = "#f2f4f2"
 
-# Creating a window 
+# Creating a window ------------------------------------------------------------------
 
 window = Tk()
+
+# Window title
+
 window.title('')
+
+# Window dimensions
+
 window.geometry('250x400')
 window.configure(background=c1)
 window.resizable(width=FALSE, height=FALSE)
 
+# Window style
+
 style = ttk.Style(window)
 style.theme_use('clam')
 
-window.mainloop()
+# Sections ------------------------------------------------------------------
 
+upFrame = Frame(window, width = 300, height = 50, bg=c1, relief = 'flat')
+upFrame.grid(row=0, column=0)
+
+mdFrame = Frame(window, width = 300, height = 90, bg=c2, relief = 'solid')
+mdFrame.grid(row=1, column=0)
+
+dnFrame = Frame(window, width = 300, height = 290, bg=c3, relief = 'raised')
+dnFrame.grid(row=2, column=0)
+
+# Logo ------------------------------------------------------------------
+
+img = Image.open('logo.png')
+img = img.resize((40,40))
+img = ImageTk.PhotoImage(img)
+
+
+
+
+
+
+# Run window
+
+window.mainloop()
